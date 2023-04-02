cacc116d711f7562203776d4dc9b0f2eaed94bbb
VadBorovec <112804701+VadBorovec@users.noreply.github.com>
2023-04-02 20:44:23 +0300

    add js for color-switcher

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,1 +1,33 @@
+const refs = {
+  startBtn: document.querySelector('button[data-start]'),
+  stopBtn: document.querySelector('button[data-stop]'),
+  body: document.querySelector('body'),
+};
 
+refs.startBtn.addEventListener('click', onStartClick);
+refs.stopBtn.addEventListener('click', onStopClick);
+
+const SWITCH_DELAY = 1000;
+let timerId = null;
+let isStarted = false;
+
+function onStartClick() {
+  if (isStarted) {
+    return;
+  }
+
+  timerId = setInterval(() => {
+    getRandomHexColor();
+    refs.body.style.backgroundColor = getRandomHexColor();
+  }, SWITCH_DELAY);
+}
+
+function onStopClick() {
+  clearInterval(timerId);
+}
+
+function getRandomHexColor() {
+  return `#${Math.floor(Math.random() * 16777215)
+    .toString(16)
+    .padStart(6, 0)}`;
+}
